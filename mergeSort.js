73fbef1ebc91dc34a5b6fdffaf58244f1bf33aae
samuelssnider <sam_redins@yahoo.com>
2017-11-26 22:11:08 -0700

    not working yet

--- mergeSort.js
+++ mergeSort.js
@@ -2,10 +2,10 @@ function mergeSort(input){
   var split = []
   for(var i = 0; i < input.length; i += 2){
     if(input[i+1]){
-      split.unshift([input[i], input[i + 1]]);
+      split.push([input[i], input[i + 1]]);
     }
     else{
-      split.unshift([input[i]]);
+      split.push([input[i]]);
     }
   }
   
@@ -18,8 +18,21 @@ function mergeSort(input){
       }
     }
   }
-  
-  return split
+  rebuild = []
+  // while(rebuild.length < 1){
+    middle = Math.ceil(split.length / 2);
+    // console.log(middle);
+    if(input.length % 2 == 0){
+      for(var k = 0; k < (split.length - 1); k += 2){
+        if(split[k][0] > split[k + 1][0]){
+          rebuild.push(split[k + 1][0]);
+        }
+      }
+    }
+    else{
+    }
+  // }
+  return rebuild;
 }
 
 module.exports = mergeSort
--- test.js
+++ test.js
@@ -22,7 +22,7 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#mergeSort()', function() {
     it('should return a sorted array with three numbers', function(){
-      assert.deepEqual([1,2,3], mergeSort([3,2,1]));
+      assert.deepEqual([1,2,3,4], mergeSort([4,3,2,1]));
     });
   });
 });
@@ -30,7 +30,7 @@ describe('Array', function() {
 describe('Array', function() {
   describe('#mergeSort()', function() {
     it('should return a sorted array with 5 numbers', function(){
-      assert.deepEqual([1,2,3,4,5], mergeSort([5,4,3,2,1]));
+      assert.deepEqual([1,2,3,4,5,6], mergeSort([6,5,4,3,2,1]));
     });
   });
 });
